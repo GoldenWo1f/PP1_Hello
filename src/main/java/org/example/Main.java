4e87f3cf7547b0facdf4656767cd387a6e60005c
GoldenWo1f <816dong@handong.ac.kr>
2023-09-01 16:51:15 +0900

    Create project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student("22200035", "곽동우");
+        System.out.println("Hello world!");
+        System.out.println(s1.toString());
+    }
+}
